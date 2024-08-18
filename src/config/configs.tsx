ed8c39a193fd398b540b9a627d43573d5dbe3e47
ShyamaCeramics <dipanshusainikhurja1998@gmail.com>
2024-08-18 21:34:25 +0530

    Delete src directory

--- src/config/configs.tsx
+++
@@ -1,4 +0,0 @@
-export const themeColor = 'orange';
-export const basePath = 'https://www.shyamaceramics.com/';
-export const adminRoutes = ['admin', 'admin/users', 'admin/orders'];
-export const userRoutes = ['orders', 'cart'];
